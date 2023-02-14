84bf741f1e7c0f7b3e0ca6c1d60b2955740f84fc
AnpilogovSergei <anpilogove.s@gmail.com>
2023-02-14 19:33:52 +0200

    Initial Commit

---
+++ AccountingForFinance/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  AccountingForFinance
+//
+//  Created by Сергей Анпилогов on 14.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+//    @IBOutlet weak var displayLabel: UILabel!
+//
+//    @IBOutlet weak var numberFromKeyBoard: [UIButton]! {
+//        didSet {
+//            for button in numberFromKeyBoard {
+//                button.layer.cornerRadius = 10
+//            }
+//        }
+//    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+//    @IBAction func numberPressed(_ sender: UIButton) {
+//        let number = sender.currentTitle!
+//        
+//        displayLabel.text = displayLabel.text? + number
+//    }
+}
+
